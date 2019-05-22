a3f43c0f8b88cef1091cc5fad88fca84f767723e
Leon Glass <lg@glasys.co.uk>
2019-05-22 14:49:13 +0100

    created canvas for display

--- src/app/maze-display/maze-display.component.ts
+++ src/app/maze-display/maze-display.component.ts
@@ -2,8 +2,13 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'maze-display',
-  templateUrl: './maze-display.component.html',
-  styleUrls: ['./maze-display.component.css']
+  template: '<canvas #canvas><canvas>',
+  styles: [`canvas{
+      border: 1px solid #000;
+      width: 100%;
+      height: 100%
+    }`
+  ]
 })
 export class MazeDisplayComponent implements OnInit {
 
